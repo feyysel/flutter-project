e941605d2bbd279c30792858a90a8d8f45edf599
feyysel <feyslawel82@gmail.com>
2026-04-21 07:34:50 -0700

    auth edit

---
+++ lib/screens/passenger/passenger_home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PassengerHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Passenger Home")),
+      body: Center(child: Text("Passenger Dashboard")),
+    );
+  }
+}
